3e668f44b9f40f9b8a89237756736cb57197b886
Myrmex <dfusi@hotmail.com>
2023-02-06 10:43:12 +0100

    Corpus editor

--- projects/myrmidon/pythia-corpus-list/src/lib/components/corpus-editor/corpus-editor.component.ts
+++ projects/myrmidon/pythia-corpus-list/src/lib/components/corpus-editor/corpus-editor.component.ts
@@ -19,8 +19,8 @@ export interface EditedCorpus extends Corpus {
 }
 
 /**
- * Corpus editor. This allows users to edit the corpus title and
- * description, plus eventually add to its contents the contents
+ * Corpus editor. This allows users to edit the corpus ID, title,
+ * and description, plus eventually add to its contents the contents
  * of another corpus. In this case, users can lookup only corpora
  * belonging to them as source, unless they are admin's.
  */
@@ -31,6 +31,8 @@ export interface EditedCorpus extends Corpus {
 })
 export class CorpusEditorComponent {
   private _corpus: EditedCorpus | undefined;
+  private _sourceId?: string;
+  public readonly idPrefix: string;
 
   /**
    * The corpus to edit.
@@ -53,10 +55,10 @@ export class CorpusEditorComponent {
   @Output()
   public editorClose: EventEmitter<any>;
 
-  public id: string | undefined;
+  public id: FormControl<string>;
   public title: FormControl<string | null>;
   public description: FormControl<string | null>;
-  public sourceId: FormControl<string | null>;
+  public clone: FormControl<boolean>;
   public form: FormGroup;
 
   public baseFilter?: CorpusFilter;
@@ -68,20 +70,31 @@ export class CorpusEditorComponent {
   ) {
     this.corpusChange = new EventEmitter<EditedCorpus>();
     this.editorClose = new EventEmitter<any>();
+    // ID prefix for new IDs
+    this.idPrefix = authService.currentUserValue?.userName || '';
+    if (this.idPrefix) {
+      this.idPrefix += '_';
+    }
     // form
+    this.id = formBuilder.control('', {
+      validators: [
+        Validators.required,
+        Validators.maxLength(50 - this.idPrefix.length),
+      ],
+      nonNullable: true,
+    });
     this.title = formBuilder.control(null, [
       Validators.required,
       Validators.maxLength(100),
     ]);
     this.description = formBuilder.control(null, Validators.maxLength(1000));
-    this.sourceId = formBuilder.control(null, [
-      Validators.required,
-      Validators.maxLength(50),
-    ]);
+    // TODO: conditional validation for clone
+    this.clone = formBuilder.control(false, { nonNullable: true });
     this.form = formBuilder.group({
+      id: this.id,
       title: this.title,
       description: this.description,
-      sourceId: this.sourceId,
+      clone: this.clone,
     });
     // preset userId filter for corpus lookup (used in cloner)
     this.baseFilter = {
@@ -91,21 +104,37 @@ export class CorpusEditorComponent {
     };
   }
 
+  /**
+   * Parse the specified corpus ID by extracting its conventional prefix
+   * (=username_). The prefix is a convention used to avoid user-scoped
+   * corpora to clash among different users.
+   *
+   * @param id The corpus ID.
+   * @returns Array of 2 strings where [0]=prefix and [1]=ID.
+   */
+  private parseId(id?: string | null): string[] {
+    if (!id) {
+      return ['', ''];
+    }
+    const i = id.indexOf('_');
+    return i === -1 ? ['', id] : [id.substring(0, i), id.substring(i + 1)];
+  }
+
   private updateForm(corpus: Corpus | undefined): void {
+    this._sourceId = undefined;
     if (!corpus) {
-      this.id = undefined;
       this.form.reset();
       return;
     }
-    this.id = corpus.id;
+    const pi = this.parseId(corpus.id);
+    this.id.setValue(pi[1]);
     this.title.setValue(corpus.title);
     this.description.setValue(corpus.description);
-    this.sourceId.reset();
     this.form.markAsPristine();
   }
 
   public onCorpusChange(corpus: Corpus | null): void {
-    this.sourceId.setValue(corpus?.id || null);
+    this._sourceId = corpus?.id || undefined;
   }
 
   private getCorpus(): EditedCorpus {
@@ -113,9 +142,10 @@ export class CorpusEditorComponent {
     // like userId must be preserved
     return {
       ...this._corpus!,
+      id: this.idPrefix + this.id.value.trim(),
       title: this.title.value?.trim() || this._corpus!.title,
       description: this.description.value?.trim() || '',
-      sourceId: this.sourceId.value || undefined,
+      sourceId: this.clone.value ? this._sourceId : undefined,
     };
   }
 
