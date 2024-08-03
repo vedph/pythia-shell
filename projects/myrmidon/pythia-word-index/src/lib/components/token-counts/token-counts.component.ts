a5fe9298a26c04214dbfa1710326f5a2690224e8
Myrmex <dfusi@hotmail.com>
2024-08-03 20:25:38 +0200

    Token counts actions

--- projects/myrmidon/pythia-word-index/src/lib/components/token-counts/token-counts.component.ts
+++ projects/myrmidon/pythia-word-index/src/lib/components/token-counts/token-counts.component.ts
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { NgxEchartsModule } from 'ngx-echarts';
@@ -27,7 +28,7 @@ import { PercentagePipe } from '../../pipes/percentage.pipe';
     MatSelectModule,
     MatTooltipModule,
     NgxEchartsModule,
-    PercentagePipe
+    PercentagePipe,
   ],
   templateUrl: './token-counts.component.html',
   styleUrl: './token-counts.component.scss',
@@ -55,6 +56,9 @@ export class TokenCountsComponent {
 
   public chartOptions: EChartsOption | null = null;
   public total = 0;
+  public downloading?: boolean;
+
+  constructor(private _clipboard: Clipboard, private _snackbar: MatSnackBar) {}
 
   private updateChart() {
     // calculate total count
@@ -94,11 +98,50 @@ export class TokenCountsComponent {
     };
   }
 
-  public copy(): void {
-    // TODO
+  private getCSV(): string {
+    // get a CSV representation of the counts
+    let csv = 'attr_name,attr_value,nr,percent\n';
+    for (const count of this._counts) {
+      csv += `${this.attribute?.name},${count.attributeValue},${count.value},${
+        count.value / this.total
+      }\n`;
+    }
+    return csv;
   }
 
-  public save(): void {
-    // TODO
+  public copy(): void {
+    if (!this._counts.length) {
+      return;
+    }
+
+    const csv = this.getCSV();
+    this._clipboard.writeText(csv);
+    this._snackbar.open('Copied to clipboard', undefined, { duration: 2000 });
+  }
+
+  public download(): void {
+    if (this.downloading || !this._counts.length) {
+      return;
+    }
+    this.downloading = true;
+
+    let csv = this.getCSV();
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    const element = document.createElement('a');
+    const file = new Blob([csv], { type: 'text/csv' });
+    element.href = URL.createObjectURL(file);
+    element.download = `${year}${month}${day}_${hours}${minutes}${seconds}.csv`;
+
+    document.body.appendChild(element);
+    element.click();
+
+    this.downloading = false;
   }
 }
